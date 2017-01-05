6c37e513cb21465b366d2a783f635652c3d19305
sudipta <basaks@gmail.com>
2017-01-05 12:18:31 +1100

    config filled with trend following parameters

--- marketcrush/config.py
+++ marketcrush/config.py
@@ -14,7 +14,18 @@ class Config:
         with open(yaml_file, 'r') as f:
             s = yaml.load(f)
         self.data_path = s['data'][0]['path']
-
+        self.strategy = s['strategy']
+        self.point_value = self.strategy['point_value']
+        self.short_tp = self.strategy['time_periods']['short_tp']
+        self.long_tp = self.strategy['time_periods']['long_tp']
+        self.filter_fp = self.strategy['time_periods']['filter_fp']
+        self.filter_sp = self.strategy['time_periods']['filter_sp']
+        self.risk_factor = self.strategy['risk_factor']
+        self.initial_cap = self.strategy['initial_cap']
+        self.atr_exit_fraction = self.strategy['atr_exit_fraction']
+        self.atr_stops_period = self.strategy['atr_stops_period']
+        self.commission = self.strategy['commission']
+        self.point_value = self.strategy['point_value']
 
 
 
